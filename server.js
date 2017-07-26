28dd603d0ec1694d737c099a18457ed9feddfdbf
Benga AKingbe <g.akingbe@gmail.com>
2017-07-25 21:17:35 -0400

    Add logging

--- server.js
+++ server.js
@@ -17,4 +17,8 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
-app.listen(port);
+
+
+app.listen(port, function() {
+	console.log("Listening on port: " + port);
+});
